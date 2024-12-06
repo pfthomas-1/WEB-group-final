1dbf34599d9c7cf83873989aa13783fbfb8c06cd
pfthomas-1 <145382532+pfthomas-1@users.noreply.github.com>
2024-12-06 09:13:42 -0500

    Update index.js
    Put basic outline of website in index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+/*
+Website code
+ - Title at the top
+ - gameArea in div below title
+ - gameArea has 3 states: menu, gamePlay, and gameOver
+   - menu state shows highscore and start button with a background of the road. start button enters gameplay state
+   - gamePlay state is the actual gameplay, getting hit enters gameover state
+   - gameOver freezes the screen and displays "restart" and "menu" buttons, which put the game in the gameplay and menu states respectively
+ - Credits and stuff beneath the gameArea
+*/
